13273dd20213ff5f92220c4eaa6382b77bb42b24
白神遥真可爱 <292200693@qq.com>
2025-02-22 22:44:25 +0800

    整理代码

--- MHDF-Tools-Bukkit/src/main/java/cn/ChengZhiYa/MHDFTools/command/feature/CustomMenu.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/ChengZhiYa/MHDFTools/command/feature/CustomMenu.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("unused")
@@ -46,6 +47,9 @@ public final class CustomMenu extends AbstractCommand {
 
     @Override
     public List<String> tabCompleter(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) {
-        return CustomMenuConfigUtil.getCustomMenuList();
+        if (args.length == 1) {
+            return CustomMenuConfigUtil.getCustomMenuList();
+        }
+        return new ArrayList<>();
     }
 }
