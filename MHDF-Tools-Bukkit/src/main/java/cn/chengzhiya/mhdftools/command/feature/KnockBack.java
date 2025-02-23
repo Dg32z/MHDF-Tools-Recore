a52dd5c2f5e3abe05bfbfbb814c6abfaa71b60e4
Dg32z_ <a1410521748@hotmail.com>
2025-02-23 13:20:23 +0800

    Fixed not work for knockback command

--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/command/feature/KnockBack.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/command/feature/KnockBack.java
@@ -1,6 +1,7 @@
 package cn.chengzhiya.mhdftools.command.feature;
 
 import cn.chengzhiya.mhdftools.command.AbstractCommand;
+import cn.chengzhiya.mhdftools.util.BungeeCordUtil;
 import cn.chengzhiya.mhdftools.util.config.ConfigUtil;
 import cn.chengzhiya.mhdftools.util.config.LangUtil;
 import org.bukkit.Bukkit;
@@ -8,6 +9,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public final class KnockBack extends AbstractCommand {
@@ -38,14 +42,22 @@ public final class KnockBack extends AbstractCommand {
             return;
         }
 
-        double x = ConfigUtil.getConfig().getDouble("vector.x");
-        double y = ConfigUtil.getConfig().getDouble("vector.y");
-        double z = ConfigUtil.getConfig().getDouble("vector.z");
+        double x = ConfigUtil.getConfig().getDouble("knockBackSettings.vector.x");
+        double y = ConfigUtil.getConfig().getDouble("knockBackSettings.vector.y");
+        double z = ConfigUtil.getConfig().getDouble("knockBackSettings.vector.z");
 
         Vector vector = new Vector(x, y, z);
         player.setVelocity(vector);
-        sender.sendMessage(LangUtil.i18n("commands.knockback.sendDone")
+        sender.sendMessage(LangUtil.i18n("commands.knockback.message")
                 .replace("{player}", player.getName())
         );
+    }
+
+    @Override
+    public List<String> tabCompleter(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) {
+        if (args.length == 1) {
+            return BungeeCordUtil.getPlayerList();
+        }
+        return new ArrayList<>();
     }
 }
