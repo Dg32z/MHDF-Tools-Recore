911c2ef644c16e833931850b048d259f2366673d
白神遥真可爱 <292200693@qq.com>
2025-03-02 14:28:07 +0800

    移除 跨服延迟传送 增加 跨服传送重试

--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/listener/misc/TpLocation.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/listener/misc/TpLocation.java
@@ -4,12 +4,17 @@ import cn.chengzhiya.mhdftools.Main;
 import cn.chengzhiya.mhdftools.entity.BungeeCordLocation;
 import cn.chengzhiya.mhdftools.listener.AbstractListener;
 import cn.chengzhiya.mhdftools.util.config.ConfigUtil;
-import cn.chengzhiya.mhdftools.util.runnable.MHDFRunnable;
+import cn.chengzhiya.mhdftools.util.scheduler.MHDFScheduler;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.HashMap;
+
 public final class TpLocation extends AbstractListener {
+    private final HashMap<String, Integer> autoTryHashMap = new HashMap<>();
+
     public TpLocation() {
         super(null);
     }
@@ -23,24 +28,40 @@ public final class TpLocation extends AbstractListener {
             return;
         }
 
-        MHDFRunnable runnable = new MHDFRunnable() {
-            @Override
-            public void run() {
-                Main.instance.getCacheManager().remove(player.getName() + "_tpLocation");
-                BungeeCordLocation tpLocation = new BungeeCordLocation(tpLocationBase64);
-                if (!tpLocation.getServer().equals(Main.instance.getBungeeCordManager().getServerName())) {
-                    return;
-                }
+        Main.instance.getCacheManager().remove(player.getName() + "_tpLocation");
 
-                player.teleport(tpLocation.toLocation());
-            }
-        };
-
-        if (!ConfigUtil.getConfig().getBoolean("bungeeCordSettings.teleportDelay.enable")) {
-            runnable.run();
+        BungeeCordLocation tpLocation = new BungeeCordLocation(tpLocationBase64);
+        if (!tpLocation.getServer().equals(Main.instance.getBungeeCordManager().getServerName())) {
             return;
         }
 
-        runnable.runTaskLater(Main.instance, ConfigUtil.getConfig().getInt("bungeeCordSettings.teleportDelay.delay"));
+        teleport(player, tpLocation.toLocation());
+    }
+
+    /**
+     * 传送玩家
+     *
+     * @param player   被传送的玩家实例
+     * @param location 传送到的位置实例
+     */
+    private void teleport(Player player, Location location) {
+        boolean result = player.teleport(location);
+        if (result) {
+            autoTryHashMap.remove(player.getName());
+            return;
+        }
+
+        int times = autoTryHashMap.get(player.getName()) != null ? autoTryHashMap.get(player.getName()) : 0;
+        if (times >= ConfigUtil.getConfig().getInt("bungeeCordSettings.autoTry.maxTimes")) {
+            autoTryHashMap.remove(player.getName());
+            return;
+        }
+
+        autoTryHashMap.put(player.getName(), times + 1);
+
+        MHDFScheduler.getGlobalRegionScheduler().runDelayed(Main.instance, (task) ->
+                        teleport(player, location),
+                ConfigUtil.getConfig().getInt("bungeeCordSettings.autoTry.delay")
+        );
     }
 }
--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/listener/misc/TpPlayer.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/listener/misc/TpPlayer.java
@@ -3,13 +3,17 @@ package cn.chengzhiya.mhdftools.listener.misc;
 import cn.chengzhiya.mhdftools.Main;
 import cn.chengzhiya.mhdftools.listener.AbstractListener;
 import cn.chengzhiya.mhdftools.util.config.ConfigUtil;
-import cn.chengzhiya.mhdftools.util.runnable.MHDFRunnable;
+import cn.chengzhiya.mhdftools.util.scheduler.MHDFScheduler;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.HashMap;
+
 public final class TpPlayer extends AbstractListener {
+    private final HashMap<String, Integer> autoTryHashMap = new HashMap<>();
+
     public TpPlayer() {
         super(null);
     }
@@ -23,24 +27,40 @@ public final class TpPlayer extends AbstractListener {
             return;
         }
 
-        MHDFRunnable runnable = new MHDFRunnable() {
-            @Override
-            public void run() {
-                Main.instance.getCacheManager().remove(player.getName() + "_tpPlayer");
-                Player targetPlayer = Bukkit.getPlayer(targetPlayerName);
-                if (targetPlayer == null) {
-                    return;
-                }
+        Main.instance.getCacheManager().remove(player.getName() + "_tpPlayer");
 
-                player.teleport(targetPlayer);
-            }
-        };
-
-        if (!ConfigUtil.getConfig().getBoolean("bungeeCordSettings.teleportDelay.enable")) {
-            runnable.run();
+        Player targetPlayer = Bukkit.getPlayer(targetPlayerName);
+        if (targetPlayer == null) {
             return;
         }
 
-        runnable.runTaskLater(Main.instance, ConfigUtil.getConfig().getInt("bungeeCordSettings.teleportDelay.delay"));
+        teleport(player, targetPlayer);
+    }
+
+    /**
+     * 传送玩家
+     *
+     * @param player       被传送的玩家实例
+     * @param targetPlayer 传送到的玩家实例
+     */
+    private void teleport(Player player, Player targetPlayer) {
+        boolean result = player.teleport(targetPlayer);
+        if (result) {
+            autoTryHashMap.remove(player.getName());
+            return;
+        }
+
+        int times = autoTryHashMap.get(player.getName()) != null ? autoTryHashMap.get(player.getName()) : 0;
+        if (times >= ConfigUtil.getConfig().getInt("bungeeCordSettings.autoTry.maxTimes")) {
+            autoTryHashMap.remove(player.getName());
+            return;
+        }
+
+        autoTryHashMap.put(player.getName(), times + 1);
+
+        MHDFScheduler.getGlobalRegionScheduler().runDelayed(Main.instance, (task) ->
+                        teleport(player, targetPlayer),
+                ConfigUtil.getConfig().getInt("bungeeCordSettings.autoTry.delay")
+        );
     }
 }
