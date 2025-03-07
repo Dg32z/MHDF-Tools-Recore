b9079b470863207f8c7bae8f93766bc3bf9e4bef
白神遥真可爱 <292200693@qq.com>
2025-03-07 13:04:44 +0800

    现在传送点命令可以后台执行了

--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/command/feature/Warp.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/command/feature/Warp.java
@@ -6,6 +6,7 @@ import cn.chengzhiya.mhdftools.entity.data.WarpData;
 import cn.chengzhiya.mhdftools.util.config.ConfigUtil;
 import cn.chengzhiya.mhdftools.util.config.LangUtil;
 import cn.chengzhiya.mhdftools.util.database.WarpDataUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -20,15 +21,35 @@ public final class Warp extends AbstractCommand {
                 "warpSettings.enable",
                 "传送到指定传送点",
                 "mhdftools.commands.warp",
-                true,
+                false,
                 ConfigUtil.getConfig().getStringList("warpSettings.commands").toArray(new String[0])
         );
     }
 
     @Override
-    public void execute(@NotNull Player sender, @NotNull String label, @NotNull String[] args) {
+    public void execute(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) {
+        Player player = null;
+
+        // 传送自己玩家至传送点
+        if (args.length == 1 && sender instanceof Player) {
+            player = (Player) sender;
+        }
+
+        // 传送其他玩家至传送点
+        if (args.length >= 2) {
+            if (Bukkit.getPlayer(args[1]) == null) {
+                sender.sendMessage(LangUtil.i18n("playerOffline"));
+                return;
+            }
+            if (!sender.hasPermission("mhdftools.commands.warp.other")) {
+                sender.sendMessage(LangUtil.i18n("noPermission"));
+                return;
+            }
+            player = Bukkit.getPlayer(args[1]);
+        }
+
         // 输出帮助信息
-        if (args.length != 1) {
+        if (player == null) {
             sender.sendMessage(LangUtil.i18n("usageError")
                     .replace("{usage}", LangUtil.i18n("commands.warp.usage"))
                     .replace("{command}", label)
@@ -60,6 +81,9 @@ public final class Warp extends AbstractCommand {
             return WarpDataUtil.getHomeDataList().stream()
                     .map(WarpData::getWarp)
                     .toList();
+        }
+        if (args.length == 2) {
+            return null;
         }
         return new ArrayList<>();
     }
