0200cea3d0beb9b8df003251000064a2dff5bf4a
白神遥真可爱 <292200693@qq.com>
2025-03-24 21:37:51 +0800

    增加 自杀确认

--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/command/feature/Suicide.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/command/feature/Suicide.java
@@ -20,21 +20,28 @@ public final class Suicide extends AbstractCommand {
 
     @Override
     public void execute(@NotNull Player sender, @NotNull String label, @NotNull String[] args) {
-        // 输出帮助信息
-        if (args.length != 0) {
-            ActionUtil.sendMessage(sender, LangUtil.i18n("usageError")
-                    .replace("{usage}", LangUtil.i18n("commands.suicide.usage"))
-                    .replace("{command}", label)
-            );
-            return;
-        }
-
         if (ConfigUtil.getConfig().getStringList("suicideSettings.blackWorld").contains(sender.getWorld().getName())) {
             ActionUtil.sendMessage(sender, LangUtil.i18n("blackWorld"));
             return;
         }
 
-        sender.setHealth(0.0);
-        ActionUtil.sendMessage(sender, LangUtil.i18n("commands.suicide.message"));
+        if (args.length == 0) {
+            ActionUtil.sendMessage(sender, LangUtil.i18n("commands.suicide.confirm"));
+            return;
+        }
+
+        if (args.length == 1) {
+            if (args[0].equals("confirm")) {
+                sender.setHealth(0.0);
+                ActionUtil.sendMessage(sender, LangUtil.i18n("commands.suicide.message"));
+                return;
+            }
+        }
+
+        // 输出帮助信息
+        ActionUtil.sendMessage(sender, LangUtil.i18n("usageError")
+                .replace("{usage}", LangUtil.i18n("commands.suicide.usage"))
+                .replace("{command}", label)
+        );
     }
 }
