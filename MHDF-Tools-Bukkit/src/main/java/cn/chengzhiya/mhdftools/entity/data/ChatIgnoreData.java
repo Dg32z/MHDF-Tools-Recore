87e3969537d0757dc8d393da8470c52b7cdfcb5a
白神遥真可爱 <292200693@qq.com>
2025-03-25 21:31:34 +0800

    修复 启动报错 的问题

--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/entity/data/ChatIgnoreData.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/entity/data/ChatIgnoreData.java
@@ -14,8 +14,8 @@ import java.util.UUID;
 public final class ChatIgnoreData extends AbstractDao {
     @DatabaseField(generatedId = true, canBeNull = false)
     private Integer id;
-    @DatabaseField(canBeNull = false, index = true)
+    @DatabaseField(canBeNull = false)
     private UUID player;
-    @DatabaseField(canBeNull = false, index = true)
+    @DatabaseField(canBeNull = false)
     private UUID ignore;
 }
