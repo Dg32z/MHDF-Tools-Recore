19ec8f1d552c41e533fc9d30e8426fda913d82a2
白神遥真可爱 <292200693@qq.com>
2025-03-25 21:21:40 +0800

    修复 不初始化表的问题

--- MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/entity/data/ChatIgnoreData.java
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/entity/data/ChatIgnoreData.java
@@ -1,5 +1,6 @@
 package cn.chengzhiya.mhdftools.entity.data;
 
+import cn.chengzhiya.mhdftools.entity.AbstractDao;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 import lombok.Getter;
@@ -10,7 +11,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @DatabaseTable(tableName = "mhdftools_chatignore")
-public final class ChatIgnoreData {
+public final class ChatIgnoreData extends AbstractDao {
     @DatabaseField(generatedId = true, canBeNull = false)
     private Integer id;
     @DatabaseField(canBeNull = false, index = true)
