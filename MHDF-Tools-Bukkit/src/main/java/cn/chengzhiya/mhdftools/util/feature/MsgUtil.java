73550a156286ef33f01d0a49886e66f463825c33
白神遥真可爱 <292200693@qq.com>
2025-03-30 13:37:23 +0800

    整理代码 格式化代码 修复 部分命令没有权限 的问题 增加 回复命令

---
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/util/feature/MsgUtil.java
@@ -0,0 +1,73 @@
+package cn.chengzhiya.mhdftools.util.feature;
+
+import cn.chengzhiya.mhdftools.Main;
+import cn.chengzhiya.mhdftools.util.action.ActionUtil;
+import cn.chengzhiya.mhdftools.util.config.ConfigUtil;
+import cn.chengzhiya.mhdftools.util.config.LangUtil;
+import cn.chengzhiya.mhdftools.util.message.ColorUtil;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.CommandSender;
+
+import java.util.regex.Pattern;
+
+public final class MsgUtil {
+    public static void sendMsg(CommandSender sender, String target, String message) {
+        // 聊天延迟
+        if (ConfigUtil.getConfig().getBoolean("chatSettings.delay.enable")) {
+            if (!sender.hasPermission("mhdftools.bypass.chat.delay")) {
+                String delayData = Main.instance.getCacheManager().get(sender.getName() + "_delay");
+                if (delayData != null) {
+                    ActionUtil.sendMessage(sender, LangUtil.i18n("chat.delay")
+                            .replace("{delay}", delayData)
+                    );
+                    return;
+                }
+            }
+        }
+
+        // 限制使用颜色符号
+        if (!sender.hasPermission("mhdftools.bypass.chat.color")) {
+            message = ChatColor.stripColor(ColorUtil.legacyColor(message));
+        }
+
+        // 限制使用miniMessage
+        if (!sender.hasPermission("mhdftools.bypass.chat.minimessage")) {
+            Pattern pattern = Pattern.compile("</?[a-zA-Z0-9_:-]+>");
+            message = pattern.matcher(message).replaceAll("");
+        }
+
+        // 刷屏限制
+        if (ConfigUtil.getConfig().getBoolean("chatSettings.spam.enable")) {
+            if (!sender.hasPermission("mhdftools.bypass.chat.spam")) {
+                String spamData = Main.instance.getCacheManager().get(sender.getName() + "_spam");
+                if (spamData != null && spamData.equals(message)) {
+                    ActionUtil.sendMessage(sender, LangUtil.i18n("chat.spam"));
+                    return;
+                }
+            }
+        }
+
+        int delay = ConfigUtil.getConfig().getInt("chatSettings.delay.delay");
+        Main.instance.getCacheManager().put(sender.getName() + "_delay", String.valueOf(delay));
+        Main.instance.getCacheManager().put(sender.getName() + "_spam", message);
+
+        // 替换词
+        if (!sender.hasPermission("mhdftools.bypass.chat.replaceWord")) {
+            message = ChatUtil.applyBlackWord(message);
+        }
+
+        Main.instance.getCacheManager().put(sender.getName() + "_reply", target);
+        Main.instance.getCacheManager().put(target + "_reply", sender.getName());
+
+        ActionUtil.sendMessage(sender, LangUtil.i18n("commands.msg.send")
+                .replace("{player}", sender.getName())
+                .replace("{target}", target)
+                .replace("{message}", message)
+        );
+        Main.instance.getBungeeCordManager().sendMessage(target, LangUtil.i18n("commands.msg.receive")
+                .replace("{player}", sender.getName())
+                .replace("{target}", target)
+                .replace("{message}", message)
+        );
+    }
+}
---
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/util/message/MessageUtil.java
@@ -0,0 +1,56 @@
+package cn.chengzhiya.mhdftools.util.message;
+
+public final class MessageUtil {
+    /**
+     * 拼接数组文本
+     *
+     * @param strings 文本数组
+     * @param origin  数组起点
+     * @param append  拼接内容
+     * @return 拼接后的文本
+     */
+    public static String mergeString(String[] strings, int origin, String append) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = origin; i < strings.length; i++) {
+            stringBuilder.append(strings[i]);
+            if (i < strings.length - 1) {
+                stringBuilder.append(append);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 拼接数组文本
+     *
+     * @param strings 文本数组
+     * @param append  拼接内容
+     * @return 拼接后的文本
+     */
+    public static String mergeString(String[] strings, String append) {
+        return mergeString(strings, 0, append);
+    }
+
+    /**
+     * 拼接数组文本
+     *
+     * @param strings 文本数组
+     * @param origin  数组起点
+     * @return 拼接后的文本
+     */
+    public static String mergeString(String[] strings, int origin) {
+        return mergeString(strings, origin, " ");
+    }
+
+    /**
+     * 拼接数组文本
+     *
+     * @param strings 文本数组
+     * @return 拼接后的文本
+     */
+    public static String mergeString(String[] strings) {
+        return mergeString(strings, 0, " ");
+    }
+}
