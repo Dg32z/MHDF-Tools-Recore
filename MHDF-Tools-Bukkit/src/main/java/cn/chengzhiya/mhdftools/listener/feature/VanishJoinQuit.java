ee03ccdaeeb7785f9d03caac91be99a71a9ce8bf
Dg32z_ <a1410521748@hotmail.com>
2025-04-09 13:38:37 +0800

    增加在隐身的时候取消加入/退出的消息

---
+++ MHDF-Tools-Bukkit/src/main/java/cn/chengzhiya/mhdftools/listener/feature/VanishJoinQuit.java
@@ -0,0 +1,38 @@
+package cn.chengzhiya.mhdftools.listener.feature;
+
+import cn.chengzhiya.mhdftools.entity.data.VanishStatus;
+import cn.chengzhiya.mhdftools.listener.AbstractListener;
+import cn.chengzhiya.mhdftools.util.database.VanishStatusUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public final class VanishJoinQuit extends AbstractListener {
+    public VanishJoinQuit() {
+        super(
+                "vanishSettings.enable"
+        );
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+     Player player = event.getPlayer();
+        VanishStatus vanishStatus = VanishStatusUtil.getVanishStatus(player);
+        if (!vanishStatus.isEnable()) {
+            return;
+        }
+
+        event.setJoinMessage(null);
+    }
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        VanishStatus vanishStatus = VanishStatusUtil.getVanishStatus(player);
+        if (!vanishStatus.isEnable()) {
+            return;
+        }
+
+        event.setQuitMessage(null);
+    }
+}
